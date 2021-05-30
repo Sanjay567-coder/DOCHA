95efe50d1162e2b77009acedefd5705cbd628fb0
Sanjay Srinivas <72558978+Sanjay567-coder@users.noreply.github.com>
2021-05-30 19:44:37 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,14 +1,14 @@
 import firebase from 'firebase';
 require('@firebase/firestore');
 
-var firebaseConfig = {
-    apiKey: "AIzaSyDUb2UTJTP1wQcafywhYxT_j8pnq09cHkg",
-    authDomain: "charitydonationapp.firebaseapp.com",
-    projectId: "charitydonationapp",
-    storageBucket: "charitydonationapp.appspot.com",
-    messagingSenderId: "221741681232",
-    appId: "1:221741681232:web:ed922d350e4f069ca31f55"
-  };
+const firebaseConfig = {
+  apiKey: "AIzaSyDUb2UTJTP1wQcafywhYxT_j8pnq09cHkg",
+  authDomain: "charitydonationapp.firebaseapp.com",
+  projectId: "charitydonationapp",
+  storageBucket: "charitydonationapp.appspot.com",
+  messagingSenderId: "221741681232",
+  appId: "1:221741681232:web:ed922d350e4f069ca31f55"
+};
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
 
